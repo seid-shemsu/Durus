a603ca34c96ae4260342541408fcbce0d0b7ff4d
ZiadeWeld <ziade.w@keplerlab.io>
2023-09-04 20:01:12 +0300

    Initial commit

---
+++ app/src/main/java/et/islam/durus/reader/Reader.java
@@ -0,0 +1,41 @@
+package et.islam.durus.reader;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnErrorListener;
+
+import java.io.File;
+
+import et.islam.durus.R;
+
+public class Reader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader);
+        /*WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///" + getIntent().getExtras().getString("file"));*/
+        PDFView pdfView = findViewById(R.id.pdf);
+        File file = new File(getIntent().getExtras().getString("file"));
+        pdfView.fromFile(file)
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .onError(new OnErrorListener() {
+                    @Override
+                    public void onError(Throwable t) {
+                        Toast.makeText(Reader.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .load();
+    }
+
+}
---
+++ app/src/main/java/et/islam/durus/ustaz/UstazActivity.java
@@ -0,0 +1,66 @@
+package et.islam.durus.ustaz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import et.islam.durus.R;
+import et.islam.durus.admob.AdMob;
+
+public class UstazActivity extends AppCompatActivity {
+    private List<UstazObject> ustazObjects = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    DatabaseReference databaseReference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(getResources().getString(R.string.app_name));
+        setContentView(R.layout.activity_ustaz);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        progressBar = findViewById(R.id.progress_bar);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Durus").child("ustazs");
+        addUstaz();
+    }
+
+    private void addUstaz() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ustazObjects.clear();
+                for (final DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String name = snapshot.child("name").getValue().toString();
+                    String img_url = snapshot.child("img").getValue().toString();
+                    ustazObjects.add(new UstazObject(name, img_url));
+                }
+                UstazAdapter ustazAdapter = new UstazAdapter(UstazActivity.this, ustazObjects);
+                recyclerView.setAdapter(ustazAdapter);
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UstazActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
